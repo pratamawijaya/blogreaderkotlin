bce474a8bd17ef482d6be357328fbf581fa72222
pratamawijaya <set.mnemonix@gmail.com>
2017-05-08 14:36:49 +0700

    show post title

--- app/src/main/kotlin/com/pratamawijaya/blogreaderkotlin/presentation/ui/home/MainActivity.kt
+++ app/src/main/kotlin/com/pratamawijaya/blogreaderkotlin/presentation/ui/home/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.pratamawijaya.blogreaderkotlin.presentation.ui.home
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.Toast
 import com.pratamawijaya.blogreaderkotlin.R.layout
 import com.pratamawijaya.blogreaderkotlin.domain.entity.Post
 import com.pratamawijaya.blogreaderkotlin.presentation.base.BaseActivity
 import com.pratamawijaya.blogreaderkotlin.presentation.di.component.AppComponent
+import com.pratamawijaya.blogreaderkotlin.presentation.ui.home.adapter.MainAdapter
 import com.pratamawijaya.blogreaderkotlin.presentation.ui.home.di.DaggerMainComponent
 import com.pratamawijaya.blogreaderkotlin.presentation.ui.home.di.MainModule
 import com.pratamawijaya.blogreaderkotlin.presentation.ui.home.presenter.MainPresenter
+import kotlinx.android.synthetic.main.activity_main.loading
+import kotlinx.android.synthetic.main.activity_main.rvContent
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -17,12 +23,26 @@ class MainActivity : BaseActivity(), MainView {
   @Inject
   lateinit var presenter: MainPresenter
 
+  var posts = arrayListOf<Post>()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(layout.activity_main)
 
     presenter.attachView(this)
+    setupRecyclerView()
     presenter.getListPost(1, true)
+  }
+
+  private fun setupRecyclerView() {
+    rvContent.layoutManager = LinearLayoutManager(this)
+    rvContent.adapter = MainAdapter(this, posts) {
+      clickPost(it)
+    }
+  }
+
+  private fun clickPost(post: Post) {
+    Toast.makeText(this, "title " + post.title, Toast.LENGTH_SHORT)
   }
 
   override fun buildComponent(appComponent: AppComponent) {
@@ -33,20 +53,26 @@ class MainActivity : BaseActivity(), MainView {
   }
 
   override fun showLoading() {
+    rvContent.visibility = GONE
+    loading.visibility = VISIBLE
   }
 
   override fun hideLoading() {
+    rvContent.visibility = VISIBLE
+    loading.visibility = GONE
   }
 
   override fun showMessage(message: String?) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
   }
 
 
   override fun displayData(t: List<Post>) {
     for (post in t) {
       Timber.d("title %s", post.title)
-      Toast.makeText(this, "title " + post.title, Toast.LENGTH_SHORT)
+      posts.add(post)
     }
+    rvContent.adapter.notifyDataSetChanged()
   }
 
 }
---
+++ app/src/main/kotlin/com/pratamawijaya/blogreaderkotlin/presentation/ui/home/adapter/MainAdapter.kt
@@ -0,0 +1,41 @@
+package com.pratamawijaya.blogreaderkotlin.presentation.ui.home.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pratamawijaya.blogreaderkotlin.R
+import com.pratamawijaya.blogreaderkotlin.domain.entity.Post
+import com.pratamawijaya.blogreaderkotlin.presentation.ui.home.adapter.MainAdapter.MainHolder
+import kotlinx.android.synthetic.main.item_post.view.tvTitle
+
+/**
+ * Created by pratama
+ * Date : May - 5/8/17
+ * Project Name : BlogReaderKotlin
+ */
+class MainAdapter(val context: Context, val posts: List<Post>,
+    val listener: (Post) -> Unit) : RecyclerView.Adapter<MainHolder>() {
+
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder = MainHolder(
+      LayoutInflater.from(context).inflate(R.layout.item_post, parent, false))
+
+  override fun onBindViewHolder(holder: MainHolder, position: Int) = holder.bindItem(
+      posts[position], listener)
+
+  override fun getItemCount(): Int = posts.size
+
+
+  class MainHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bindItem(post: Post, listener: (Post) -> Unit) = with(itemView) {
+      tvTitle.text = post.title
+
+      setOnClickListener { listener(post) }
+    }
+
+
+  }
+}
